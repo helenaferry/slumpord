a5b50845b6ec4bb7acd16657e323133babdc9498
Helena Ferry <helena.ferry@gmail.com>
2023-05-17 20:26:49 +0200

    Slumpordsapp!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// @ts-nocheck
+
+	import { nouns } from '../nouns';
+	import { adjectives } from '../adjectives';
+	import { verbs } from './../verbs';
+	import { whenOrWhere } from './../whenOrWhere';
+
+	let nounsPos;
+	let adjectivesPos;
+	let verbsPos;
+	let whenOrWherePos;
+
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * max);
+	}
+
+	function randomize() {
+		nounsPos = getRandomInt(nouns.length - 1);
+		adjectivesPos = getRandomInt(adjectives.length - 1);
+		verbsPos = getRandomInt(verbs.length - 1);
+		whenOrWherePos = getRandomInt(whenOrWhere.length - 1);
+	}
+
+	randomize();
+</script>
+
+<section>
+	<h1>Slumpord till skrivövningar</h1>
+	<p>{nouns[nounsPos]}</p>
+
+	<p>{adjectives[adjectivesPos]}</p>
+
+	<p>{verbs[verbsPos]}</p>
+
+	<p>{whenOrWhere[whenOrWherePos]}</p>
+
+	<p>
+		<button on:click={randomize}>Nya ord, tack!</button>
+	</p>
+</section>
---
+++ src/verbs.ts
@@ -0,0 +1,18 @@
+export const verbs = [
+    'vaknar',
+    'regnar',
+    'gnistrar',
+    'palla',
+    'prata',
+    'vinna',
+    'förlora',
+    'beräkna',
+    'återta',
+    'erövra',
+    'fundera',
+    'hitta',
+    'leta',
+    'sakna',
+    'frita',
+    'flörta'
+];
